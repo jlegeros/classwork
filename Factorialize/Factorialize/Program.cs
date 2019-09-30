b3969c6e4e52898e641b3b4cffffb56dfea5aa58
jlegeros <jlegeros@gmail.com>
2019-09-30 11:37:01 -0400

    freecodecamp factorialize in C#

---
+++ Factorialize/Factorialize/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Factorialize
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Factorialize.number(5);
+            Factorialize.number(10);
+            Factorialize.number(20);
+            Factorialize.number(0);
+        }
+    }
+
+    public static class Factorialize
+    {
+        public static void number(int num)
+        {
+            long product = 1;
+            for (int i = 2; i <= num; i++)
+            {
+                product = product * i;
+            }
+            Console.WriteLine(product);
+        }
+    }
+}
