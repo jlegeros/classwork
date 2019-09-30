0d4a2e09845908ebeb411888a148b22516f34320
jlegeros <jlegeros@gmail.com>
2019-09-30 11:23:39 -0400

    freecodecamp string reversal in C#

---
+++ ReverseAString/ReverseAString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReverseAString
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MyString.reverseString("Test");
+            MyString.reverseString("hello");
+            MyString.reverseString("Howdy");
+            MyString.reverseString("Greetings from Earth");
+        }
+    }
+
+    public static class MyString
+    {
+        public static void reverseString(string arg)
+        {
+            var reversedChars = new char[arg.Length];
+            var count = 0;
+            for (int i = arg.Length - 1; i >= 0; i--)
+            {
+                reversedChars[count] = arg[i];
+                count++;
+            }
+            Console.WriteLine(reversedChars);
+        }
+    }
+}
