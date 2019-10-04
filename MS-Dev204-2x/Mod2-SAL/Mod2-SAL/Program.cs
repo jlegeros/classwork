7fa0b070ef3eccb7108d6520a497bcadc7095bd8
jlegeros <jlegeros@gmail.com>
2019-10-04 12:33:56 -0400

    Module 3 of edX OOP in C#

---
+++ MS-Dev204-2x/Mod2-SAL/Mod2-SAL/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Mod2SAL
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var program = new UProgram("Information Technology");
+            var student1 = new Student("Erica", 14);
+            var student2 = new Student("Tom", 15);
+            var student3 = new Student("Mary", 16);
+            var course = new Course("Programming with C#");
+
+            course.AddStudent(student1);
+            course.AddStudent(student2);
+            course.AddStudent(student3);
+
+            var teacher = new Teacher("Marco", 45);
+
+            course.AddTeacher(teacher);
+
+            var degree = new Degree("Bachelor");
+
+            degree.AddCourse(course);
+
+            program.AddDegree(degree);
+
+            Console.WriteLine($"{program.Name} contains {program.GetDegrees()}");
+            Console.WriteLine($"{degree.Name} contains {degree.GetCourses()}");
+            Console.WriteLine($"There are {course.GetStudentCount()} students enrolled in {course.Name}.");
+            Console.WriteLine(student1.Status());
+            Console.WriteLine(student2.Status());
+            Console.WriteLine(student3.Status());
+            Console.WriteLine(teacher.Status());
+        }
+    }
+}
---
+++ MS-Dev204-2x/Mod3-Lab1/Mod3-Lab1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace Mod3Lab1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Creates StreamReader object called 'streamReaderObject'
+            // Assigns its value to null
+            StreamReader streamReaderObject = null;
+
+            try
+            {
+                // Assigns streamReaderObject to read from a text file named 'file1.txt'
+                streamReaderObject = new StreamReader("file1.txt");
+
+                // Reads all characters from the current position to the end pf the stream
+                // Stores in String variable 'contents'
+                string contents = streamReaderObject.ReadToEnd();
+
+                // Closes the StreadReader
+                streamReaderObject.Close();
+
+                // Writes the amount of text elements in the text file to the Console
+                Console.WriteLine($"The file has {new StringInfo(contents).LengthInTextElements} text elements.");
+            }
+            // Code to handle any errors
+            catch (FileNotFoundException)
+            {
+                // Informs user there is no file to read
+                Console.WriteLine("The file cannot be found");
+            }
+            // Invoking the Dispose method in a finally block
+            // note that code in finally block will always execute
+            finally
+            {
+                // Checks if object is not null
+                if (streamReaderObject != null)
+                    streamReaderObject.Dispose();
+            }
+        }
+    }
+}
