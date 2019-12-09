d702d94ef29aabfb6968275db949e308167fb571
jlegeros <jlegeros@gmail.com>
2019-12-09 13:52:41 -0500

    intial binary tree commit

---
+++ binary-tree.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// create a binary node
+// node should have an integer value
+// node should have two branches
+
+int main(void)
+{
+  printf("hello\n");
+}
