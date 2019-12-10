73e5fca8c8c6af69ad23e28dcc0e9c8323f2d702
jlegeros <jlegeros@gmail.com>
2019-12-10 10:27:11 -0500

    Intial add node functionality

--- binary-tree.c
+++ binary-tree.c
@@ -5,7 +5,59 @@
 // node should have an integer value
 // node should have two branches
 
+typedef struct BINARYNODE
+{
+  int value;
+  struct BINARYNODE *greater;
+  struct BINARYNODE *lesser;
+} BINARYNODE;
+
+BINARYNODE *root = NULL;
+
+void addNode(int val)
+{
+  BINARYNODE *newNode = malloc(sizeof(BINARYNODE));
+  newNode->value = val;
+  newNode->greater = NULL;
+  newNode->lesser = NULL;
+
+  if (root == NULL)
+  {
+    root = newNode;
+    printf("Added %i node to the root\n", val);
+  }
+  else
+  {
+    BINARYNODE *trav = root;
+    int count = 1;
+
+    if (trav->value < newNode->value)
+    {
+      while (trav->greater != NULL)
+      {
+        trav = trav->greater;
+        count++;
+      }
+      trav->greater = newNode;
+      printf("Added %i Node to left side, %i branches down\n", val, count);
+    }
+    else
+    {
+      while (trav->lesser != NULL)
+      {
+        trav->lesser = newNode;
+        count++;
+      }
+      trav->lesser = newNode;
+      printf("Added %i Node to right side, %i branches down\n", val, count);
+    }
+  }
+}
+
 int main(void)
 {
-  printf("hello\n");
+  addNode(7);
+  addNode(9);
+  addNode(4);
+  addNode(11);
 }
